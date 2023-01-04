d574a7849b744bb176ee421eaeccd6c2f3b6f92d
Kass Daniello <zertroz@gmail.com>
2023-01-04 17:00:30 -0500

    Add save palette framework

--- scripts.js
+++ scripts.js
@@ -1,9 +1,16 @@
-var currentPalette 
 var randomColorsSection = document.querySelector('.random-colors')
 var newPaletteButton = document.querySelector('#new-palette-button')
+var savedPalettesSection = document.querySelector('.saved-palettes')
+var savePalettesButton = document.querySelector('#save-palette-button')
+
+
+var currentPalette
+var savedPalettes = []
+
 
 window.addEventListener("load", createPalette)
 newPaletteButton.addEventListener('click', createNewPalette)
+savePalettesButton.addEventListener('click', savePalette)
 
 function createHexCode() {
     var hexCharacters = ["A", "B", "C", "D", "E", "F", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
@@ -27,14 +34,14 @@ function generateColors() {
    randomColorsSection.innerHTML = ''
    for (var i = 0; i < 5; i++) {
     randomColorsSection.innerHTML += 
-    `<div class="boxes" id="swatch${[i]}">
-      <div id="box-${[i]}"></div>
-      <div class="hex-locks">
-        <p class="label" id="label-${[i]}">${currentPalette.colors[i].hexCode}</p>
-        <p class="locks" id="unlocked-${[i]}">🔓</p>
-        <p class="locks hidden" id="locked-${[i]}">🔒</p>
-      </div>
-    </div>`
+        `<div class="boxes" id="swatch${[i]}">
+            <div id="box-${[i]}"></div>
+            <div class="hex-locks">
+                <p class="label" id="label-${[i]}">${currentPalette.colors[i].hexCode}</p>
+                <p class="locks" id="unlocked-${[i]}">🔓</p>
+                <p class="locks hidden" id="locked-${[i]}">🔒</p>
+            </div>
+        </div>`
     document.getElementById(`box-${[i]}`).style.backgroundColor = `${currentPalette.colors[i].hexCode}`
    }
 }
@@ -44,4 +51,10 @@ function createNewPalette() {
         currentPalette.colors[i].hexCode = createHexCode()
     }
     generateColors()
+}
+
+function savePalette() {
+    savedPalettes.push(currentPalette)
+    createPalette()
+    
 }
